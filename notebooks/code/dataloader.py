d933a7a36f0dc884cd69673bb0b7ab342e74a0df
bsuleymanov <contact@bsuleymanov.ml>
2021-04-07 00:09:50 +0500

    add loader returning.

--- notebooks/code/dataloader.py
+++ notebooks/code/dataloader.py
@@ -78,5 +78,4 @@ class CelebAMaskHQLoader:
             dataset=dataset, batch_size=self.batch_size,
             shuffle=True if self.mode == "train" else False,
             num_workers=2, drop_last=False)
-
         return loader
